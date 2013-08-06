ee27e30645b2be4f3e58c64543a88ba3ea108230
Al Scott <al.scott@agencyrepublic.com>
2013-08-06 12:04:31 +0100

    A few minor tweaks and added compass import config
    renamed and restructured slightly
    
    added some content in index
    
    added compass module config
    
    ignored all cache files

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/public/stylesheets"
+css_dir = "/public/css"
 sass_dir = "/sass"
 images_dir = "/public/images"
 javascripts_dir = "/public/js"
---
+++ public/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<!-- HTML5 Boilerplate -->
+<!--[if lt IE 7]><html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html class="lt-ie9 lt-ie8 ie7" lang="en"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html class="lt-ie9 ie8" lang="en"><![endif]-->
+<!--[if (IE 9)&!(IEMobile)]><html class="ie9" lang="en"><![endif]-->
+<!--[if gt IE 9]><!--> <html lang="en"><!--<![endif]-->
+	<head>
+		<!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    	<link href="/css/ie.css" rel="stylesheet">
+	    <![endif]-->
+	    
+	    <!--[if gte IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <link rel="stylesheet" media="screen" type="text/css" href="/css/screen.css">
+	    <![endif]-->
+	    
+	    <!--[if !IE]><!-->
+        <link rel="stylesheet" media="screen" type="text/css" href="/css/screen.css">
+	    <!--<![endif]-->
+	    
+	    <link rel="stylesheet" media="print" type="text/css" href="/css/print.css">
+	</head>
+	<body>
+		<main>
+			<section>
+				<h1>SASSquatch</h1>
+
+				<p>This is SASSquatch.</p>
+
+				<img width="50%" src="/images/sassquatch.png" alt="SASSquatch">
+			</section>
+		</main>
+	</body>
+</html>
