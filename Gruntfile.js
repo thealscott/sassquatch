ffb2e6314b922163aadb8a3e00db3192fed9ebbd
Al Scott <al.scott@agencyrepublic.com>
2013-08-15 17:00:33 +0100

    added a setup method, and refactored the other tasks slightly

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,75 +11,100 @@ module.exports = function(grunt) {
         dest: 'build/<%= pkg.name %>.min.js'
       }
     },
-    add_page: {
-      options: {
-        breakpoints : ['480', '768', '960']
-      }
-    },
-    add_module: {
-      options: {
-
-      } 
+    sassquatch: {
+      breakpoints : [
+        // base always exists
+        '480',
+        '768', 
+        '960'
+      ],
+      pages : [
+        // default always exists
+        'test',
+        'test_two'
+      ],
+      modules : [
+        'test_mod',
+        'test_mod_two'
+      ]
     }
   });
 
-  grunt.registerTask('add_page', 'Add a new page to SASSquatch project', function(page_name) {
-    grunt.log.write('New page: "'+ page_name + '"');
+  var sassquatch_methods = {
+    'setup' : function() {
+      grunt.log.write('Running full SASSquatch setup, from config JSON');
 
-    var config = grunt.config.get('add_page'),
-        breakpoints = config.options.breakpoints,
-        length = breakpoints.length,
-        content = '@import "base";\r\n';
+      var config = grunt.config.get('sassquatch');
 
-    breakpoints.forEach(function(value, index){
-      content += '@import "'+ value +'";\r\n';
+      config.pages.forEach(function(value, index){
+        sassquatch_methods.add_page(value);
+      });
 
-      var local_content = ''+
-      '@mixin '+ page_name +'_'+ value +'() {\r\n' +
-      '}';
+      config.modules.forEach(function(value, index){
+        sassquatch_methods.add_module(value);
+      });
+
+    },
+    'add_page' : function(page_name) {
+      grunt.log.write('New page: "'+ page_name + '"');
+
+      var config = grunt.config.get('sassquatch'),
+          breakpoints = config.breakpoints,
+          content = '@import "base";\r\n';
+
+      breakpoints.forEach(function(value, index){
+        content += '@import "'+ value +'";\r\n';
+
+        var local_content = ''+
+        '@mixin '+ page_name +'_'+ value +'() {\r\n' +
+        '}';
 
 
-      grunt.file.write('sass/pages/'+ page_name +'/_'+ value +'.scss', local_content);
-      grunt.log.write('Added breakpoint: "'+ value + '"').ok();
-    });
+        grunt.file.write('sass/pages/'+ page_name +'/_'+ value +'.scss', local_content);
+        grunt.log.write('Added breakpoint: "'+ value + '"').ok();
+      });
 
-    grunt.file.write('sass/pages/'+ page_name +'/_base.scss', '@mixin '+ page_name +'_base() {\r\n}');
-    grunt.log.write('Added breakpoint: "base"').ok();
+      grunt.file.write('sass/pages/'+ page_name +'/_base.scss', '@mixin '+ page_name +'_base() {\r\n}');
+      grunt.log.write('Added breakpoint: "base"').ok();
 
-    grunt.file.write('sass/pages/'+ page_name +'/_'+ page_name+ '.scss', content);
-    grunt.log.write('Added page: "'+ page_name + '"').ok();
+      grunt.file.write('sass/pages/'+ page_name +'/_'+ page_name+ '.scss', content);
+      grunt.log.write('Added page: "'+ page_name + '"').ok();
 
 
-    var imports_config = grunt.file.read('sass/config/_imports.scss');
-    imports_config += '\r\n@import "pages/'+ page_name +'/'+ page_name +'";';
-    
-    grunt.file.write('sass/config/_imports.scss', imports_config);
+      var imports_config = grunt.file.read('sass/config/_imports_pages.scss');
+      imports_config += '\r\n@import "pages/'+ page_name +'/'+ page_name +'";';
+      
+      grunt.file.write('sass/config/_imports_pages.scss', imports_config);
 
-    grunt.log.write('Added line to config/_imports for "'+ page_name + '"').ok();
+      grunt.log.write('Added line to config/_imports_pages for "'+ page_name + '"').ok();
 
-  });
+    },
+    'add_module' : function(module_name) {
+      grunt.log.write('New module: "'+ module_name + '"');
 
-  grunt.registerTask('add_module', 'Add a new module to SASSquatch project', function(module_name) {
-    grunt.log.write('New module: "'+ module_name + '"');
+      var content = ''+
+        '@mixin '+ module_name +'() {\r\n' +
+        '    %'+ module_name +'_example {\r\n'+
+        '    }\r\n'+
+        '}';
+      
+      grunt.file.write('sass/modules/_'+ module_name +'.scss', content);
 
-    var content = ''+
-      '@mixin '+ module_name +'() {\r\n' +
-      '    %'+ module_name +'_example {\r\n'+
-      '    }\r\n'+
-      '}';
-    
-    grunt.file.write('sass/modules/_'+ module_name +'.scss', content);
+      grunt.log.write('Added module: "'+ module_name + '"').ok();
 
-    grunt.log.write('Added module: "'+ module_name + '"').ok();
+      var imports_config = grunt.file.read('sass/config/_imports_modules.scss');
+      imports_config += '\r\n@import "modules/'+ module_name +'";';
 
-    var imports_config = grunt.file.read('sass/config/_imports.scss');
-    imports_config += '\r\n@import "modules/'+ module_name +'";';
+      grunt.file.write('sass/config/_imports_modules.scss', imports_config);
 
-    grunt.file.write('sass/config/_imports.scss', imports_config);
+      grunt.log.write('Added line to config/_imports_modules for "'+ module_name + '"').ok();
+    }
+  }
 
-    grunt.log.write('Added line to config/_imports for "'+ module_name + '"').ok();
-    
 
-  });
+
+  grunt.registerTask('add_page', 'Add a new page to SASSquatch project', sassquatch_methods.add_page);
+  grunt.registerTask('add_module', 'Add a new module to SASSquatch project', sassquatch_methods.add_module);
+  grunt.registerTask('setup', 'Setup a full SASSquatch project based on config JSON', sassquatch_methods.setup);
 
 };
